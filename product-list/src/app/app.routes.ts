4884a2dfb69459b4c4ed6fbece98f29dccdb36ba
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-07-19 11:14:23 +0200

    add 404 page

--- product-list/src/app/app.routes.ts
+++ product-list/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
 import { ProductList } from './shared/components/product-list/product-list';
+import { PageNotFound } from './page-not-found/page-not-found';
+
 // import { Component } from '@angular/compiler';
 // import { CompA } from './comp-a/comp-a';
 // import { CompB } from './comp-b/comp-b';
@@ -8,19 +10,27 @@ import { ProductList } from './shared/components/product-list/product-list';
 export const routes: Routes = [
   { path: '', component: ProductList },
   { path: 'detail/:name', component: ProductDetail },
-
-
-  // {path: "compc",
-  //   redirectTo: "CompB",
-  // },
-  // { path: '**', redirectTo: "" },
-
-  //   {
-  // path: "",
-  // component: CompA
-  //   },
-  //   {
-  // path: "CompB/:uid",
-  // component: CompB
-  //   },
+  { path: 'page-not-found', component: PageNotFound },
+  { path: '**', redirectTo: 'page-not-found' },
 ];
+
+// export const routes: Routes = [
+//   { path: 'detail/:name', component: ProductDetail, PageNotFound },
+//   { path: '', component: ProductList },
+
+//   { path: '**', redirectTo: 'PageNotFound' },
+
+// {path: "compc",
+//   redirectTo: "CompB",
+// },
+// { path: '**', redirectTo: "" },
+
+//   {
+// path: "",
+// component: CompA
+//   },
+//   {
+// path: "CompB/:uid",
+// component: CompB
+//   },
+// ];
